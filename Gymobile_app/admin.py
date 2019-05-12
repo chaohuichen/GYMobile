29bfee17cb1a8abe10b3314ce40e8ad92f90b433
Peter(ChaoHui) Chen <chenpeter0821@gmail.com>
2019-05-12 15:02:53 -0400

    add models

--- Gymobile_app/admin.py
+++ Gymobile_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from Gymobile_app.models import Day,workout
+
+admin.site.register(Day)
+admin.site.register(workout)
--- Gymobile_app/models.py
+++ Gymobile_app/models.py
@@ -1,11 +1,24 @@
 from django.db import models
-
+from datetime import datetime,timedelta
 # Create your models here.
-class Topic(models.Model):
-    "..."
-    text = models.CharField(max_length=200)
-    data_added = models.DateTimeField(auto_now_add=True)
+class Day(models.Model):
+    "A day the user is having"
+    #day = models.DateTimeField(default=datetime.now()+timedelta(days=30))
+    day= models.DateTimeField(default=datetime.now())
 
     def __str__(self):
         "..return"
-        return self.text
+        return str(self.day)
+
+class workout(models.Model):
+    "workout type"
+    day = models.ForeignKey(Day,on_delete=models.CASCADE)
+    exercise = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'workouts'
+
+    def __str__(self):
+        "return"
+        return self.exercise[:50]+"..."
