b3783ae5be79aa426211715909cc2874653ef158
Peter Chen <peterchen@Peters-MacBook-Air.local>
2019-11-04 18:37:12 -0500

    fix folder

--- Gymobile/Gymblog/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class GymblogConfig(AppConfig):
-    name = 'Gymblog'
--- Gymobile/Gymobile_app/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from Gymobile_app.models import WorkoutLog,Exercies,Routine
-
-admin.site.register(Routine)
-admin.site.register(WorkoutLog)
-admin.site.register(Exercies)
