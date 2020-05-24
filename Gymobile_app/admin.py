8624a8aaec952cc627a4eef450c0f6c592a68431
lhcheung <47724121+lhcheung@users.noreply.github.com>
2020-05-23 20:00:15 -0400

    clean up

--- Gymobile_app/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from Gymobile_app.models import WorkoutLog,Exercise,Routine
-
-admin.site.register(Routine)
-admin.site.register(WorkoutLog)
-admin.site.register(Exercise)
--- Gymobile_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class GymobileAppConfig(AppConfig):
-    name = 'Gymobile_app'
